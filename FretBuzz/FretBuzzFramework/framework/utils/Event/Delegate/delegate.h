af30eadabc32e0984bc106afbf6aa8526f8451f0
FretBuzzBoogie <rohan31f@gmail.com>
2020-04-04 12:42:15 +0530

    - Delegate :: Removed delegate base class, now works directly with the delegate class.

--- FretBuzz/FretBuzzFramework/framework/utils/Event/Delegate/delegate.h
+++ FretBuzz/FretBuzzFramework/framework/utils/Event/Delegate/delegate.h
@@ -1,79 +1,131 @@
 #pragma once
-#include <iostream>
-#include "delegate_base.h"
+#include "action.h"
+#include <vector>
 
 namespace ns_fretBuzz
 {
 	template<typename T> class Delegate;
 
 	template<typename T_RET_TYPE, typename ...T_ARGS>
-	class Delegate <T_RET_TYPE(T_ARGS...)> : protected DelegateBase<T_RET_TYPE(T_ARGS...)>
+	class Delegate <T_RET_TYPE(T_ARGS...)>
 	{
-	public:
-		///Add member function contained within given class type into the delagte list
-		template<typename T_CLASS_TYPE, T_RET_TYPE(T_CLASS_TYPE::* T_METHOD)(T_ARGS...)>
-		void Add(T_CLASS_TYPE* a_TObject)
-		{
-			DelegateBase<T_RET_TYPE(T_ARGS...)>::template Add<T_CLASS_TYPE, T_METHOD>(a_TObject);
-		}
+		private:
 
-		///Adds const member function contained within given class type into the delagte 
-		template<typename T_CLASS_TYPE, T_RET_TYPE(T_CLASS_TYPE::* T_METHOD)(T_ARGS...) const>
-		void Add(T_CLASS_TYPE* const a_TObject)
-		{
-			DelegateBase<T_RET_TYPE(T_ARGS...)>::template Add<T_CLASS_TYPE, T_METHOD>(a_TObject);
-		}
+			using T_ACTION = Action<T_RET_TYPE(T_ARGS...)>;
 
-		///Adds static functionn into delegate list
-		template<T_RET_TYPE(*T_METHOD)(T_ARGS...)>
-		void Add()
-		{
-			DelegateBase<T_RET_TYPE(T_ARGS...)>::template Add<T_METHOD>();
-		}
+			///Set to store all the Actions of the same type
+			std::vector<T_ACTION> m_vectActions;
 
-		///Calls the function stored in the delegate list
-		T_RET_TYPE Invoke(T_ARGS... a_Args) throw(std::out_of_range)
-		{
-			try
+		public:
+
+			///Adds a member function into the delegate list
+			template<typename T_CLASS_TYPE, T_RET_TYPE(T_CLASS_TYPE::*T_METHOD)(T_ARGS...)>
+			void Add(T_CLASS_TYPE* a_Instance)
 			{
-				return DelegateBase<T_RET_TYPE(T_ARGS...)>::Invoke(a_Args...);
+				m_vectActions.emplace_back(T_ACTION::template GetAction<T_CLASS_TYPE, T_METHOD>(a_Instance));
 			}
-			catch (std::out_of_range& a_Exception)
+
+			///Adds a const member function into the delegate list
+			template<typename T_CLASS_TYPE, T_RET_TYPE(T_CLASS_TYPE::*T_METHOD)(T_ARGS...) const>
+			void Add(T_CLASS_TYPE* const a_Instance)
 			{
-				std::cout << "DelegateError::Cannot invoke::" << a_Exception.what() << "\n";
+				m_vectActions.emplace_back(T_ACTION::template GetAction<T_CLASS_TYPE, T_METHOD>(a_Instance));
 			}
-		}
 
-		///removes a member function from the collection of actions
-		template<typename T_CLASS_TYPE, T_RET_TYPE(T_CLASS_TYPE::* T_METHOD)(T_ARGS...)>
-		void Remove(T_CLASS_TYPE* a_Instance)
-		{
-			DelegateBase<T_RET_TYPE(T_ARGS...)>::template Remove<T_CLASS_TYPE, T_METHOD>(a_Instance);
-		}
+			///Adds an static or global action into the list
+			template<T_RET_TYPE(*T_METHOD)(T_ARGS...)>
+			void Add()
+			{
+				m_vectActions.emplace_back(T_ACTION::template GetAction<T_METHOD>());
+			}
 
-		///removes a const member function from the collection of actions
-		template<typename T_CLASS_TYPE, T_RET_TYPE(T_CLASS_TYPE::* T_METHOD)(T_ARGS...) const>
-		void Remove(T_CLASS_TYPE* const a_Instance)
-		{
-			DelegateBase<T_RET_TYPE(T_ARGS...)>::template Remove<T_CLASS_TYPE, T_METHOD>(a_Instance);
-		}
+			T_RET_TYPE operator()(T_ARGS... a_Args)
+			{
+				return Invoke(a_Args...);
+			}
 
-		///removes an static or global action from the collection of actions
-		template<T_RET_TYPE(*T_METHOD)(T_ARGS...)>
-		void Remove()
-		{
-			DelegateBase<T_RET_TYPE(T_ARGS...)>::template Remove<T_METHOD>();
-		}
+			///Calls all the actions stored within the delegate
+			T_RET_TYPE Invoke(T_ARGS... a_Args) throw(std::out_of_range)
+			{
+				try
+				{
+					if (m_vectActions.size() == 0)
+					{
+						throw std::out_of_range("Delegate is empty\n");
+					}
 
-		T_RET_TYPE operator()(T_ARGS... a_Args)
-		{
-			return Invoke(a_Args...);
-		}
+					T_RET_TYPE l_tReturn;
+					for (typename std::vector<T_ACTION>::iterator l_iterator = m_vectActions.begin();
+						l_iterator != m_vectActions.end();
+						l_iterator++)
+					{
+						l_tReturn = l_iterator->Invoke(a_Args...);
+					}
 
-		///Clears the stored actions of type withing the delegate
-		void Clear()
-		{
-			DelegateBase<T_RET_TYPE(T_ARGS...)>::Clear();
-		}
+					if (typeid(T_RET_TYPE) != typeid(void))
+					{
+						return l_tReturn;
+					}
+				}
+				catch (std::out_of_range& a_Exception)
+				{
+					std::cout << "Delegate Exception::Cannot invoke::" << a_Exception.what() << "\n";
+				}
+			}
+
+			///removes a member function into the delegate list
+			template<typename T_CLASS_TYPE, T_RET_TYPE(T_CLASS_TYPE::* T_METHOD)(T_ARGS...)>
+			void Remove(T_CLASS_TYPE* a_Instance)
+			{
+				for (typename std::vector<T_ACTION>::iterator l_iterator = m_vectActions.begin();
+					l_iterator != m_vectActions.end();
+					l_iterator++)
+				{
+					if (l_iterator->isActionEqual<T_CLASS_TYPE, T_METHOD>(a_Instance))
+					{
+						m_vectActions.erase(l_iterator);
+						break;
+					}
+				}
+			}
+
+			///removes a const member function into the delegate list
+			template<typename T_CLASS_TYPE, T_RET_TYPE(T_CLASS_TYPE::* T_METHOD)(T_ARGS...) const>
+			void Remove(T_CLASS_TYPE* const a_Instance)
+			{
+				for (typename std::vector<T_ACTION>::iterator l_iterator = m_vectActions.begin();
+					l_iterator != m_vectActions.end();
+					l_iterator++)
+				{
+					if (l_iterator->isActionEqual<T_CLASS_TYPE, T_METHOD>(a_Instance))
+					{
+						m_vectActions.erase(l_iterator);
+						break;
+					}
+				}
+			}
+
+			///removes an static or global action into the list
+			template<T_RET_TYPE(*T_METHOD)(T_ARGS...)>
+			void Remove()
+			{
+				for (typename std::vector<T_ACTION>::iterator l_iterator = m_vectActions.begin();
+					l_iterator != m_vectActions.end();
+					l_iterator++)
+				{
+					if (l_iterator->isActionEqual<T_METHOD>())
+					{
+						m_vectActions.erase(l_iterator);
+						break;
+					}
+				}
+			}
+
+			///Clears the stored actions of type withing the delegate
+			void Clear()
+			{
+				m_vectActions.clear();
+			}
 	};
+
 }
