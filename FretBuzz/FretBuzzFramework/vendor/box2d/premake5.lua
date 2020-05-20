332b3e3eeae8b397508c65487c9041f7f5c148d7
FretBuzzBoogie <rohan31f@gmail.com>
2020-05-21 00:48:39 +0530

    - added submodule box2d and glfw

---
+++ FretBuzz/FretBuzzFramework/vendor/box2d/premake5.lua
@@ -0,0 +1,30 @@
+project "box2d"
+	kind "StaticLib"
+	language "C++"
+
+	targetdir ("Bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("Bin/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"box2d/include/Box2d/**.h",
+		"box2d/src/**.h",
+		"box2d/src/**.cpp"
+	}
+	
+	includedirs
+	{
+		"box2d/src/",
+		"box2d/include/"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
